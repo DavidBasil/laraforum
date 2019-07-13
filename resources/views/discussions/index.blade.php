8f8fb6c9d5c656fee41b554bcb22cc1122bd51bd
David Basil <info@basildavid.com>
2019-07-13 18:12:53 +0400

    install laravel-gravatar

--- resources/views/discussions/index.blade.php
+++ resources/views/discussions/index.blade.php
@@ -5,7 +5,6 @@
   @foreach ($discussions as $discussion)
     <div class="card mb-3">
       <div class="card-header">
-        {{ $discussion->title }}
       </div>
       <div class="card-body">
         {!! $discussion->content !!}
